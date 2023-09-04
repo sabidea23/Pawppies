61f3c07e25f68ccb62d1d111013ab2e96f4f2ca5
Sabina Dinu <dinu_sabina18@yahoo.com>
2023-09-05 00:08:25 +0300

    Initialize project

---
+++ backend/src/main/java/licenta/LicentaApplication.java
@@ -0,0 +1,13 @@
+package licenta;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LicentaApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(LicentaApplication.class, args);
+	}
+
+}
---
+++ backend/src/test/java/licenta/LicentaApplicationTests.java
@@ -0,0 +1,13 @@
+package licenta;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class LicentaApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
