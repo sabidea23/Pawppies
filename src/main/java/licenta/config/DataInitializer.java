553144be98be7528129772afe8e8b55748499249
Sabina Dinu <dinu_sabina18@yahoo.com>
2024-03-19 17:11:48 +0200

    Create roles for user and initialize admin user

--- src/main/java/licenta/config/DataInitializer.java
+++ src/main/java/licenta/config/DataInitializer.java
@@ -2,16 +2,29 @@ package licenta.config;
 
 import javax.annotation.PostConstruct;
 import licenta.entity.Role;
+import licenta.entity.User;
+import licenta.entity.UserRole;
 import licenta.repo.RoleRepository;
+import licenta.repo.UserRepository;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.stereotype.Component;
+
+import java.util.HashSet;
 
 @Component
 public class DataInitializer {
 
     private final RoleRepository roleRepository;
 
-    public DataInitializer(RoleRepository roleRepository) {
+    private final UserRepository userRepository;
+
+    private final BCryptPasswordEncoder bCryptPasswordEncoder;
+
+    public DataInitializer(RoleRepository roleRepository, UserRepository userRepository,
+                           BCryptPasswordEncoder bCryptPasswordEncoder) {
         this.roleRepository = roleRepository;
+        this.userRepository = userRepository;
+        this.bCryptPasswordEncoder = bCryptPasswordEncoder;
     }
 
     @PostConstruct
@@ -27,9 +40,38 @@ public class DataInitializer {
         if (!roleRepository.existsById(1L)) {
             Role role = Role.builder()
                     .roleId(1L)
+                    .roleName("SUPPLIER")
+                    .build();
+            roleRepository.save(role);
+        }
+        
+        if (!roleRepository.existsById(2L)) {
+            Role role = Role.builder()
+                    .roleId(2L)
                     .roleName("ADMIN")
                     .build();
             roleRepository.save(role);
+
+            String encodedPassword = this.bCryptPasswordEncoder.encode("Adminadmin23");
+            User newUser = User.builder()
+                    .username("admin")
+                    .password(encodedPassword)
+                    .firstName("admin")
+                    .lastName("admin")
+                    .email("dinu.sabina18@gmail.com")
+                    .phone("0735116377")
+                    .userRoles(new HashSet<>())
+                    .build();
+
+            User savedUser = this.userRepository.save(newUser);
+
+            UserRole userRole = UserRole.builder()
+                    .user(savedUser)
+                    .role(role)
+                    .build();
+
+            savedUser.getUserRoles().add(userRole);
+            this.userRepository.save(savedUser);
         }
     }
 }
