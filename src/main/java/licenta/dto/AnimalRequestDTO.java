7468f18410445f38dd5ab76d5336f5554bab1e62
Sabina Dinu <dinu_sabina18@yahoo.com>
2024-03-20 22:32:10 +0200

    Create DTO objects for BreedDetailsController

--- src/main/java/licenta/dto/AnimalRequestDTO.java
+++ src/main/java/licenta/dto/AnimalRequestDTO.java
@@ -1,52 +1,36 @@
 package licenta.dto;
 
-import com.sun.istack.NotNull;
+import licenta.entity.User;
 import licenta.entity.AnimalCenter;
 import licenta.entity.BreedDetails;
 import licenta.entity.ImageModel;
-import licenta.entity.User;
+import lombok.Builder;
 import lombok.Data;
-import lombok.NoArgsConstructor;
 
 import java.time.LocalDate;
 import java.util.Set;
 
 @Data
-@NoArgsConstructor
+@Builder
 public class AnimalRequestDTO {
-    
+
     private String name;
-    
     private String age;
-    
     private String gender;
-    
     private String size;
-    
     private String coatLength;
-    
     private String type;
-    
     private String health;
-    
     private String care;
-    
     private String color;
-    
     private String description;
-    
     private String goodInHome;
-    
     private LocalDate postedDate;
-    
     private Boolean isAdopted;
-    
-    private User author;
-    
-    private AnimalCenter animalCenter;
-    
-    private BreedDetails breedDetails;
-    
-    private Set<Long> liked_by;
-    private Set<ImageModel> animalImages;
+    private User author;  // Trimiterea întregului obiect User
+    private AnimalCenter animalCenter;  // Trimiterea întregului obiect AnimalCenter
+    private BreedDetails breedDetails;  // Trimiterea întregului obiect BreedDetails
+    private Set<ImageModel> animalImages;  // Setul de imagini poate fi trimis dacă este necesar
+
+    // Getters și setters pentru toate câmpurile
 }
