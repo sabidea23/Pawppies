bdc6b950d8706399113177d7910b8c8fc868f81a
Sabina Dinu <dinu_sabina18@yahoo.com>
2024-04-04 02:03:06 +0300

    Added most popular dogs breeds page

--- frontend/licenta/src/app/pages/adopt-dog/popular-dogs/popular-dogs.component.ts
+++ frontend/licenta/src/app/pages/adopt-dog/popular-dogs/popular-dogs.component.ts
@@ -1,4 +1,10 @@
-import { Component } from '@angular/core';
+import {Component} from '@angular/core';
+import {Breed_detailsService} from "../../../services/breed_details.service";
+import {MatSnackBar} from "@angular/material/snack-bar";
+import {ImageProcessingService} from "../../../services/image-processing.service";
+import {AnimalService} from "../../../services/animal.service.";
+import {LoginService} from "../../../services/login.service";
+import {Router} from "@angular/router";
 
 @Component({
   selector: 'app-popular-dogs',
@@ -6,5 +12,47 @@ import { Component } from '@angular/core';
   styleUrls: ['./popular-dogs.component.css']
 })
 export class PopularDogsComponent {
+  constructor(private breedDetailsService: Breed_detailsService, private snack: MatSnackBar, private imageProcessingService: ImageProcessingService, private animalService: AnimalService, private login: LoginService, private router: Router) {
+  }
 
+  ngOnInit() {
+    this.getAllBreeds();
+  }
+
+  isFullTextShown = false;
+
+  toggleTextDisplay() {
+    this.isFullTextShown = !this.isFullTextShown;
+  }
+
+
+  // @ts-ignore
+  redirectToAnimalPage(dog) {
+    // Presupunând că dog.breed conține rasa selectată
+    this.router.navigate(['/animal/'], {queryParams: {type: 'Dog', breed: dog.name}});
+  }
+
+  viewDogDetails(dog: any) {
+    this.router.navigate(['/breed-dog', {dogBreedId: dog.id}]).then((_) => {
+    });
+  }
+
+  cuteDogs: any = [];
+
+  cuteBreedsName = ['Pug', 'French Bulldog', 'Cavalier King Charles Spaniel', 'Golden Retriever', 'Beagle', 'Poodle', 'Boxer', 'Labrador Retriever', 'Yorkshire Terrier', 'Bulldogs']
+
+  filterBreeds() {
+    // @ts-ignore
+    this.cuteDogs = this.cuteDogs.filter(animal => this.cuteBreedsName.includes(animal.name.trim()))
+  }
+
+  getAllBreeds() {
+    this.breedDetailsService.getBreedDetailsByAnimalType("DOG")
+      .subscribe(data => {
+        this.cuteDogs = data;
+        this.filterBreeds(); // Aplică orice filtru existent
+      }, error => {
+        console.log(error.error.message);
+      });
+  }
 }
