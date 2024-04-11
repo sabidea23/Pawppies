178ebbf910ab00749ae6be759ba07b867c4487c8
Sabina Dinu <dinu_sabina18@yahoo.com>
2024-04-11 12:03:31 +0300

    Added resources in navbar

--- frontend/licenta/src/app/pages/administration/navbar/navbar.component.ts
+++ frontend/licenta/src/app/pages/administration/navbar/navbar.component.ts
@@ -1,53 +1,43 @@
-import { Component, OnInit } from '@angular/core';
-import { LoginService } from 'src/app/services/login.service';
-import { Router, Scroll } from '@angular/router';
+import {Component, OnInit} from '@angular/core';
+import {LoginService} from 'src/app/services/login.service';
+import {Router, Scroll} from '@angular/router';
 import {NotificationService} from "../../../services/notification.service";
 
 
 @Component({
-  selector: 'app-navbar',
-  templateUrl: './navbar.component.html',
-  styleUrls: ['./navbar.component.css'],
+  selector: 'app-navbar', templateUrl: './navbar.component.html', styleUrls: ['./navbar.component.css'],
 
 })
 export class NavbarComponent implements OnInit {
   isLoggedIn = false;
   user: any = null;
 
-  constructor(
-    public login: LoginService,
-    public router: Router,
-
-    public notificationService: NotificationService)
-  {
-      this.router.events.subscribe((event: any) => {
-        if (event instanceof Scroll && event.anchor) {
-          setTimeout(() => {
-            this.scroll('#' + event.anchor);
-          }, 100);
-        }
-      });
+  constructor(public login: LoginService, public router: Router, public notificationService: NotificationService) {
+    this.router.events.subscribe((event: any) => {
+      if (event instanceof Scroll && event.anchor) {
+        setTimeout(() => {
+          this.scroll('#' + event.anchor);
+        }, 100);
+      }
+    });
   }
 
   unreadCount: number = 0;
-   notifications: any = [];
+  notifications: any = [];
 
 
   private getNotifications() {
-    this.notificationService.getNotificationsByUserId(this.user.id).subscribe(
-      notifications => {
-        // @ts-ignore
-        this.notifications = notifications.sort((a, b) => {
-          return new Date(b.localDate).getTime() - new Date(a.localDate).getTime();
-        });
-        this.unreadCount = notifications.filter(notification => !notification.isReadByUser).length;
-        console.log(this.notifications);
-        console.log(this.unreadCount);
-      },
-      error => {
-        console.error('There was an error retrieving the notifications', error);
-      }
-    );
+    this.notificationService.getNotificationsByUserId(this.user.id).subscribe(notifications => {
+      // @ts-ignore
+      this.notifications = notifications.sort((a, b) => {
+        return new Date(b.localDate).getTime() - new Date(a.localDate).getTime();
+      });
+      this.unreadCount = notifications.filter(notification => !notification.isReadByUser).length;
+      console.log(this.notifications);
+      console.log(this.unreadCount);
+    }, error => {
+      console.error('There was an error retrieving the notifications', error);
+    });
   }
 
 
@@ -57,15 +47,12 @@ export class NavbarComponent implements OnInit {
       // @ts-ignore
       if (!notification.isReadByUser) {
         // @ts-ignore
-        this.notificationService.userReadNotificationById(this.user.id, notification.id).subscribe(
-          () => {
-            // @ts-ignore
-            notification.isReadByUser = true;
-          },
-          error => {
-            console.error('Error marking notification as read', error);
-          }
-        );
+        this.notificationService.userReadNotificationById(this.user.id, notification.id).subscribe(() => {
+          // @ts-ignore
+          notification.isReadByUser = true;
+        }, error => {
+          console.error('Error marking notification as read', error);
+        });
       }
       this.unreadCount = 0;
     });
@@ -75,20 +62,15 @@ export class NavbarComponent implements OnInit {
   private scroll(query: string) {
     const targetElement = document.querySelector(query);
     if (!targetElement) {
-      window.scrollTo({ top: 0, behavior: 'smooth' });
+      window.scrollTo({top: 0, behavior: 'smooth'});
     } else if (!this.isInViewport(targetElement)) {
-      targetElement.scrollIntoView({ behavior: 'smooth' });
+      targetElement.scrollIntoView({behavior: 'smooth'});
     }
   }
 
   private isInViewport = (elem: any) => {
     const bounding = elem.getBoundingClientRect();
-    return (
-      bounding.top >= 0 &&
-      bounding.left >= 0 &&
-      bounding.bottom <= (window.innerHeight || document.documentElement.clientHeight) &&
-      bounding.right <= (window.innerWidth || document.documentElement.clientWidth)
-    );
+    return (bounding.top >= 0 && bounding.left >= 0 && bounding.bottom <= (window.innerHeight || document.documentElement.clientHeight) && bounding.right <= (window.innerWidth || document.documentElement.clientWidth));
   };
 
   ngOnInit(): void {
@@ -100,8 +82,7 @@ export class NavbarComponent implements OnInit {
     });
 
     let itemJson = {
-      value: 'true',
-      timestamp: new Date().getTime(),
+      value: 'true', timestamp: new Date().getTime(),
     }
     const summStorageItem = sessionStorage.getItem('initSumm');
 
@@ -119,8 +100,7 @@ export class NavbarComponent implements OnInit {
       if (timeDiffInSeconds >= 300) {
         // Update timestamp
         itemJson = {
-          value: 'true',
-          timestamp: new Date().getTime(),
+          value: 'true', timestamp: new Date().getTime(),
         }
 
         // Update the `sessionStorage` item
@@ -141,24 +121,30 @@ export class NavbarComponent implements OnInit {
 
   public toProfileSettings() {
     const user_role = this.login.getUserRole();
-      this.router.navigate(['/profile']).then((_) => { });
+    this.router.navigate(['/profile']).then((_) => {
+    });
   }
 
   public redirectAnimalCenter() {
-      this.router.navigate(['/centers']).then((_) => { });
+    this.router.navigate(['/centers']).then((_) => {
+    });
   }
 
   public redirectToAboutPawppies() {
-    this.router.navigate(['/about-pets']).then((_) => { });
+    this.router.navigate(['/about-pets']).then((_) => {
+    });
   }
 
   public redirectToDogBreeds() {
-    this.router.navigate(['/breed-details-dog']).then((_) => { });
+    this.router.navigate(['/breed-details-dog']).then((_) => {
+    });
   }
 
   public redirectToCatBreeds() {
-    this.router.navigate(['/breed-details-cat']).then((_) => { });
+    this.router.navigate(['/breed-details-cat']).then((_) => {
+    });
   }
+
   isSubMenuOpen = true;
   profileDetailMenu = false;
 
@@ -182,54 +168,93 @@ export class NavbarComponent implements OnInit {
 
   redirectToFavoriteAnimalsPage() {
     const user_role = this.login.getUserRole();
-      this.router.navigate(['/my-fav-animals',{ userId: this.user.id }]).then((_) => { });
+    this.router.navigate(['/my-fav-animals', {userId: this.user.id}]).then((_) => {
+    });
   }
 
   redirectToLoginPage() {
-    this.router.navigate(['/login']).then((_) => { });
+    this.router.navigate(['/login']).then((_) => {
+    });
   }
 
   toUserManagement() {
-    this.router.navigate(['/admin/user-management']).then((_) => { });
+    this.router.navigate(['/admin/user-management']).then((_) => {
+    });
 
   }
 
   redirectToDogAdoption() {
-    this.router.navigate(['/adopt-dogs']).then((_) => { });
+    this.router.navigate(['/adopt-dogs']).then((_) => {
+    });
 
   }
 
   redirectToFeedDog() {
-    this.router.navigate(['/feed-dog']).then((_) => { });
+    this.router.navigate(['/feed-dog']).then((_) => {
+    });
 
   }
+
   redirectToDogTraining() {
-    this.router.navigate(['/training-dog']).then((_) => { });
+    this.router.navigate(['/training-dog']).then((_) => {
+    });
 
   }
+
   redirectToDogHealth() {
-    this.router.navigate(['/health-dog']).then((_) => { });
+    this.router.navigate(['/health-dog']).then((_) => {
+    });
 
   }
+
   redirectToCatAdoption() {
-    this.router.navigate(['/adopt-cats']).then((_) => { });
+    this.router.navigate(['/adopt-cats']).then((_) => {
+    });
 
   }
+
   redirectToFeedCat() {
-    this.router.navigate(['/feed-cat']).then((_) => { });
+    this.router.navigate(['/feed-cat']).then((_) => {
+    });
 
   }
+
   redirectToCatBehavior() {
-    this.router.navigate(['/admin/behavior-cat']).then((_) => { });
+    this.router.navigate(['/admin/behavior-cat']).then((_) => {
+    });
+  }
+
+  redirectToFAQs() {
+    this.router.navigate(['/faqs']).then((_) => {
+    });
 
   }
-  redirectToCatHealth() {
-    this.router.navigate(['/health-cat']).then((_) => { });
 
+  redirectToContact() {
+    this.router.navigate(['/contact']).then((_) => {
+    });
+
+  }
+
+  redirectToPrivacyPolicy() {
+    this.router.navigate(['/privacy-policy']).then((_) => {
+    });
+
+  }
+
+  redirectToTearmsOfService() {
+    this.router.navigate(['terms-of-service']).then((_) => {
+    });
+
+  }
+
+  redirectToCatHealth() {
+    this.router.navigate(['/health-cat']).then((_) => {
+    });
   }
 
   redirectToMission() {
-    this.router.navigate(['/about-pets/mission']).then((_) => { });
-
+    this.router.navigate(['/about-pets/mission']).then((_) => {
+    });
   }
 }
