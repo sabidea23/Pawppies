015411357ae9f4afcc8239fd02aeca86600081b0
Sabina Dinu <dinu_sabina18@yahoo.com>
2024-04-17 03:32:28 +0300

    Added mat-errors for edit animals

--- frontend/licenta/src/app/animal.process/edit-animal/edit-animal.component.ts
+++ frontend/licenta/src/app/animal.process/edit-animal/edit-animal.component.ts
@@ -1,45 +1,61 @@
-import {Component, Inject} from '@angular/core';
-import {MatDialogRef, MAT_DIALOG_DATA} from '@angular/material/dialog';
-import {MapDialogComponent} from "../../utils/map-dialog/map-dialog.component";
+import { Component, Inject } from '@angular/core';
+import { MatDialogRef, MAT_DIALOG_DATA } from '@angular/material/dialog';
+import { FormGroup, FormControl, Validators } from '@angular/forms';
+import { MapDialogComponent } from "../../utils/map-dialog/map-dialog.component";
 
-// @ts-ignore
 @Component({
-  selector: 'app-edit-animal', templateUrl: './edit-animal.component.html', styleUrls: ['./edit-animal.component.css']
+  selector: 'app-edit-animal',
+  templateUrl: './edit-animal.component.html',
+  styleUrls: ['./edit-animal.component.css']
 })
 export class EditAnimalComponent {
-  editData = {
-    name: this.animal.name,
-    age: this.animal.age,
-    health: this.animal.health,
-    description: this.animal.description,
-    latitude: this.animal.latitude,
-    care: this.animal.care,
-    coatLength: this.animal.coatLength,
-    size: this.animal.size,
-    type: this.animal.type
-  };
+  editForm = new FormGroup({
+    name: new FormControl('', Validators.required),
+    age: new FormControl('', Validators.required),
+    size: new FormControl('', Validators.required),
+    coatLength: new FormControl('', Validators.required),
+    health: new FormControl('', Validators.required),
+    description: new FormControl('', Validators.required),
+  });
 
-  getAgeOptions(animalType: string): string[] {
-    if (animalType === 'Dog') {
+  constructor(
+    public dialogRef: MatDialogRef<MapDialogComponent>,
+    @Inject(MAT_DIALOG_DATA) public animal: any
+  ) {
+    this.loadInitialData();
+  }
+
+  loadInitialData() {
+    this.editForm.patchValue({
+      name: this.animal.name,
+      age: this.animal.age,
+      health: this.animal.health,
+      description: this.animal.description,
+      size: this.animal.size,
+      coatLength: this.animal.coatLength,
+    });
+  }
+
+  save() {
+    if (this.editForm.valid) {
+      this.dialogRef.close(this.editForm.value);
+    }
+  }
+
+  getAgeOptions(): string[] {
+    if (this.animal.type === 'Dog') {
       return ['Puppy', 'Young', 'Adult', 'Senior'];
-    } else if (animalType === 'Cat') {
+    } else if (this.animal.type === 'Cat') {
       return ['Kitten', 'Young', 'Adult', 'Senior'];
     } else return ['Young', 'Adult', 'Senior'];
   }
 
-  getSizeOptions(animalType: string): string[] {
-    if (animalType === 'Dog') {
+  getSizeOptions(): string[] {
+    if (this.animal.type === 'Dog') {
       return ['Small (0-25 ibs)', 'Medium (26-60 ibs)', 'Large (61-100) ibs', 'Extra Large (101 ibs or more)']; // Sunt doar exemple, pune valorile dorite
-    } else if (animalType === 'Cat') {
+    } else if (this.animal.type == 'Cat') {
       return ['Small (0-6 ibs)', 'Medium (7-11 ibs)', 'Large (12-17) ibs', 'Extra Large (18 ibs or more)'];
     }
     return ['Small=', 'Medium', 'Large', 'Extra Large'];
-  }
-
-  constructor(public dialogRef: MatDialogRef<MapDialogComponent>, @Inject(MAT_DIALOG_DATA) public animal: any) {
-  }
-
-  save() {
-    this.dialogRef.close(this.editData);
   }
 }
